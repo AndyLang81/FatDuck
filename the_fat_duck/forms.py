7758d905e77c76e3f34fb8d8d8bc3ea7834ce1f5
Anders Langhoff <Andylanghoff@gmail.com>
2024-11-27 19:26:04 +0000

    added forms.py

---
+++ the_fat_duck/forms.py
@@ -0,0 +1,9 @@
+# the_fat_duck/forms.py
+
+from django import forms
+from .models import Booking
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['name', 'email', 'phone', 'date', 'guests']
