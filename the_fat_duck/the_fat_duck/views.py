1c69f093bff731ad54698a1af0955fcecfcd70b6
Anders Langhoff <Andylanghoff@gmail.com>
2024-11-27 20:06:47 +0000

    defined forms.py and urls.py

--- the_fat_duck/the_fat_duck/views.py
+++ the_fat_duck/the_fat_duck/views.py
@@ -1,3 +1,15 @@
-# fatduck_app/views.py
+# the_fat_duck/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookingForm
+
+def book_table(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the booking data to the database
+            return redirect('booking_success')  # Redirect to a success page or confirmation page
+    else:
+        form = BookingForm()
+    
+    return render(request, 'book.html', {'form': form})
--- the_fat_duck/urls.py
+++ the_fat_duck/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book/', views.book_table, name='book_table'),  # Link the booking form to the /book/ URL
 ]
