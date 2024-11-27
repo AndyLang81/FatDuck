0394c7640e67f0871e940e02f0e7bc88e1c152ed
Anders Langhoff <Andylanghoff@gmail.com>
2024-11-27 20:14:01 +0000

    updated urls.py to submit booking

--- the_fat_duck/urls.py
+++ the_fat_duck/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views  # Import  views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('book/', views.book_table, name='book_table'),  # Link the booking form to the /book/ URL
+    path('submit-booking/', views.submit_booking, name='submit_booking'),
 ]
